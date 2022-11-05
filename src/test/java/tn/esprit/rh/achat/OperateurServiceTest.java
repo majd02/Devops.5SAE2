a55ac974d26534da9dae1c2769a213e609aabc94
seif derbali <seif.derbali@esprit.tn>
2022-11-05 18:09:07 +0100

    commit3

--- src/test/java/tn/esprit/rh/achat/OperateurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceTest.java
@@ -9,7 +9,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
